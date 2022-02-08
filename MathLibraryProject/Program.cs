e6198ef51f9e87d480a216444f2d09f72b86224d
Jeff Workman <jworkhollr@aol.com>
2022-02-08 15:57:45 -0500

    Add project files.

---
+++ MathLibraryProject/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MathLibraryProject {
+
+    public class Program {
+        static void Main(string[] args) {
+            // the var can change ie.. 1.0m, 2.0m or "1", "2" and the var will look for that type
+            var a = Math.Add("1", "2");
+        }
+    }
+}
